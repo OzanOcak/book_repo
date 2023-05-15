f35474590360f56ab64950ecf619fc27cafc1829
OzanOcak <ozanocak@gmail.com>
2023-05-15 13:23:22 +0300

    file structure

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+//const db = config.get("mongoURI");
+
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.mongoURI, {
       useNewUrlParser: true,
     });
 
